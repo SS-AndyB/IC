55f338208137e982743a0cbed74765119a53ad23
Simon Hawke <simonjhawke@gmail.com>
2015-10-28 18:58:28 +0000

    adding sample.

---
+++ spikes/OwinSample/OwinSample/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace OwinSample
+{
+    using Microsoft.Owin.Hosting;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseUrl = "http://*:5000";
+            using (WebApp.Start<Startup>(baseUrl))
+            {
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadKey();
+
+            }
+        }
+    }
+}
---
+++ spikes/OwinSample/OwinSample/Startup.cs
@@ -0,0 +1,44 @@
+﻿[assembly: Microsoft.Owin.OwinStartup(typeof(OwinSample.Startup))]
+
+namespace OwinSample
+{
+    using System;
+    using System.Reflection;
+    using System.Threading.Tasks;
+    using Microsoft.Owin;
+    using Owin;
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage(WelcomePage());
+
+            app.Run(owinContext());
+        }
+
+        private static Microsoft.Owin.Diagnostics.WelcomePageOptions WelcomePage()
+        {
+            return new Microsoft.Owin.Diagnostics.WelcomePageOptions()
+            {
+                Path = new PathString("/welcome")
+            };
+        }
+
+        private static Func<IOwinContext, Task> owinContext()
+        {
+            return context =>
+            {
+                context.Response.ContentType = "text/plain";
+
+                string output = string.Format(
+                    "Welcome to Innovation Club Live!\nI'm running on {0} \nFrom assembly {1}",
+                    Environment.OSVersion,
+                    Assembly.GetEntryAssembly().FullName
+                    );
+
+                return context.Response.WriteAsync(output);
+            };
+        }
+    }
+}
